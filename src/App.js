810d91a325690fdf137c286d582c1273153d7f89
Deonte Manning <deontemanning@Deontes-MacBook-Pro.local>
2020-08-20 10:42:59 -0400

    fixed some bugs around signing in/out.

--- src/App.js
+++ src/App.js
@@ -29,13 +29,6 @@ const particleOptions = {
         value_area: 250
       }
     },
-    // line_linked: {
-    //   shadow: {
-    //     enable: true,
-    //     color: "#3CA9D1",
-    //     blur: 5,
-    //   },
-    // },
   },
 };
 
@@ -71,12 +64,12 @@ class App extends Component {
 
   onRouteChange = (route = 'signin') => {
     if(route === 'signout' || route === 'register') {
-      this.setState({isSignedIn: false})
+      this.setState({isSignedIn: false});
     }else if (route === 'home'){
       this.setState({isSignedIn:true})
     }
 
-    this.setState({route : route})
+    this.setState({route : route});
   }
 
   calculateFaceLocation = (data) => {
@@ -127,6 +120,30 @@ class App extends Component {
   }
 
   render() {
+    const route = this.state.route;
+    let view;
+
+    if(route === 'home'){
+      view = (
+        <div>
+          <Rank name={this.state.user.name} entries={this.state.user.entries} />
+          <ImageLinkForm
+            onInputChange={this.onInputChange}
+            onSubmit={this.onSubmit}
+          />
+          <FaceRecogniton imgURL={this.state.imgURL} box={this.state.box} />
+        </div>
+      );
+    } else if (route === 'signin' || route === 'signout'){
+      view = (
+        <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+      );
+    } else if(route === 'register'){
+      view = (
+        <Register onRouteChange={this.onRouteChange} loadUser={this.loadUser} />
+      );
+    }
+
     return (
       <div className="App">
         <Particles className="particles" params={particleOptions} />
@@ -135,27 +152,7 @@ class App extends Component {
           route={this.state.route}
           isSignedIn={this.state.isSignedIn}
         />
-        {this.state.route === "home" ? (
-          <div>
-            {/* <Logo /> */}
-            <Rank
-              name={this.state.user.name}
-              entries={this.state.user.entries}
-            />
-            <ImageLinkForm
-              onInputChange={this.onInputChange}
-              onSubmit={this.onSubmit}
-            />
-            <FaceRecogniton imgURL={this.state.imgURL} box={this.state.box} />
-          </div>
-        ) : this.state.route === "signin" ? (
-          <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
-        ) : (
-          <Register
-            onRouteChange={this.onRouteChange}
-            loadUser={this.loadUser}
-          />
-        )}
+       {view}
       </div>
     );
   }
